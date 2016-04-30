f1c17efadac5a0540c83a8dcd95684726962742c
Jonathan Garvey <jon.d.garvey@gmail.com>
2016-04-29 21:41:10 -0700

    remove angular-component dependency

--- client/app/app.js
+++ client/app/app.js
@@ -4,7 +4,6 @@ import 'normalize.css';
 import angular from 'angular';
 import angularAnimate from 'angular-animate';
 import uiRouter from 'angular-ui-router';
-import angularComponent from 'angular-component';
 import Components from './components/components';
 import AppComponent from './app.component';
 import Common from './common/common';
