ff1aa1defa947239cba67b542b9dfb567bdb20e8
Lukas Ruebbelke <simpul@gmail.com>
2016-06-30 17:28:50 -0700

    component controllers complete

--- client/app/components/categories/categories.component.js
+++ client/app/components/categories/categories.component.js
@@ -1,8 +1,11 @@
 import template from './categories.html';
+import controller from './categories.controller';
 import './categories.styl';
 
 const categoriesComponent = {
-  template
+  template,
+  controller,
+  controllerAs: 'categoriesListCtrl'
 }
 
 export default categoriesComponent;
