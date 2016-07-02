1c8a300ea8bf5d7aa0a1a279b102ffd6eaa13deb
Jonathan Garvey <jon.d.garvey@gmail.com>
2016-07-01 20:46:46 -0700

    linting

--- client/app/app.js
+++ client/app/app.js
@@ -3,7 +3,7 @@ import 'normalize.css';
 
 import angular from 'angular';
 import appComponent from './app.component';
-import ComponentsModule from './components/components'
+import ComponentsModule from './components/components';
 
 angular.module('app', [
     ComponentsModule.name
