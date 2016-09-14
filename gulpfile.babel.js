090f2ae54558f14e5aae9c07f4e28e20048c8861
Jonathan Garvey <jon.d.garvey@gmail.com>
2016-09-14 10:19:50 -0700

    put browser-sync reload in separate task

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,22 +1,19 @@
-'use strict';
+import gulp from 'gulp';
+import path from 'path';
+import webpack from 'webpack-stream';
+const browserSync = require('browser-sync');
 
-import gulp     from 'gulp';
-import webpack  from 'webpack-stream';
-import path     from 'path';
-import sync     from 'run-sequence';
-import serve    from 'browser-sync';
-
-let reload = () => serve.reload();
-let root = 'client';
+const reload = () => browserSync.reload();
+const root = 'client';
 
 // helper method for resolving paths
-let resolveToApp = (glob) => {
+const resolveToApp = (glob) => {
   glob = glob || '';
   return path.join(root, 'app', glob); // app/{glob}
 };
 
 // map of all paths
-let paths = {
+const paths = {
   js: resolveToApp('**/*!(.spec.js).js'), // exclude spec files
   styl: resolveToApp('**/*.styl'), // stylesheets
   html: [
@@ -27,26 +24,28 @@ let paths = {
   output: root
 };
 
-// use webpack.config.js to build modules
 gulp.task('webpack', () => {
   return gulp.src(paths.entry)
     .pipe(webpack(require('./webpack.config')))
     .pipe(gulp.dest(paths.output));
 });
 
+gulp.task('reload', ['webpack'], (done) => {
+  reload();
+  done();
+});
+
 gulp.task('serve', () => {
-  serve({
+  browserSync({
     port: process.env.PORT || 3000,
     open: false,
     server: { baseDir: root }
   });
 });
 
-gulp.task('watch', () => {
-  let allPaths = [].concat([paths.js], paths.html, [paths.styl]);
-  gulp.watch(allPaths, ['webpack', reload]);
+gulp.task('watch', ['serve'], () => {
+  const allPaths = [].concat([paths.js], paths.html, [paths.styl]);
+  gulp.watch(allPaths, ['reload']);
 });
 
-gulp.task('default', (done) => {
-  sync('webpack', 'serve', 'watch', done);
-});
+gulp.task('default', ['watch']);
